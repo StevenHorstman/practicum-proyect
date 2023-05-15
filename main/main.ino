d1d1ff4c38a225934da6fa49c755b21df269b964
StevenHorstman <steven.horstman@gmail.com>
2023-05-15 11:38:49 -0600

    Terminado

--- main/main.ino
+++ main/main.ino
@@ -4,49 +4,78 @@
 #include "internet.h"
 #include <ArduinoJson.h>
 
-// TODO. Hacer que el te de el ssid y la constraseña se reciba por comunicacion serial
-String json = "{\"ssid\": \"Steven\", \"password\": \"12345678\", \"server-number\":3, \"servers\": [{\"type\":\"https\", \"name\":\"arduino.cc\", \"endpoint\":\"/\"},{\"type\":\"http\", \"name\":\"192.168.100.13\", \"endpoint\":\"/\"}, {\"type\":\"https\", \"name\":\"arduiterterteno.cc\", \"endpoint\":\"/\"}]}";
-StaticJsonDocument<2048> config;
 
-const int BUFFER_SIZE = 2048;
-char buf[BUFFER_SIZE];
-
-int numberOfServers = config["server_number"];
-const char ssid[] = "Totalplay-2.4G-f030";
-const char pass[] = "32W6W96ptV8mPK2P";
-
+DynamicJsonDocument config(1024);
+DynamicJsonDocument msg(256);
 
 WiFiClient client;
 HttpClient http(client);
 
 int status = WL_IDLE_STATUS;
 
-void setup(){
-  // indica el pueto donde se hace la comunicacion serial
+void setup() {
   Serial.begin(9600);
-  Serial.println("Inicio");
-  while (Serial.available() <= 0){}
-  if (Serial.available() > 0){
-    int rlen = Serial.readBytes(buf, BUFFER_SIZE);
+  msg["name"] = "ard1";
+  msg["action"] = "configuration";
 
-    Serial.print("I recived: ");
-    for (int i = 0; i < rlen; i++){
-      Serial.print(buf[i]);
+  String content = "";
+
+  internetConnection();
+
+  Serial.println("Connected to internet");
+
+  if (client.connect("192.168.100.10", 3000)) {
+    Serial.println("Connected to server");
+    // Covierte le json y lo envia a la servidor
+    serializeJson(msg, content);
+    char charBuf[256];
+    content.toCharArray(charBuf, 256);
+    client.write(charBuf);
+
+    Serial.println(charBuf);
+    
+    // Obtiene el string  Json
+    String fin = "";
+    delay(5000);
+    while(client.available()) {
+      char c = client.read();
+      fin = fin + String(c);
     }
+
+    //Deserializa el string json y lo permte deja usable
+    deserializeJson(config, fin);
+    String name = config["device_name"];
+    Serial.print(name);
+
   }
 
-  
-  DeserializationError error = deserializeJson(config, json);
-  config_print();
-  /*
-  internetConnection();
-  ipsTry();*/
-  Serial.println("Fin");
+  client.stop();
+
+  Serial.println("end");
+  serverTest();
 }
+
 
 
 void loop(){
 
+}
+
+
+String getConfiguration(){
+  int BUFFER_SIZE = 2048;
+  char buf[BUFFER_SIZE];
+  String json = "";
+ while (Serial.available() <= 0){}
+  if (Serial.available() > 0){
+    int rlen = Serial.readBytes(buf, BUFFER_SIZE);
+
+    
+    for (int i = 0; i < rlen; i++){
+      json = json + String(buf[i]);
+    }
+  }
+  return json;
 }
 
 void config_print(){
@@ -61,17 +90,17 @@ void config_print(){
     Serial.println(ser);
   }
 }
-void internetConnection() {
 
+void internetConnection() {
   while(status != WL_CONNECTED) {
-    status = WiFi.begin(config["ssid"], config["password"]);
-    Serial.println("Conetado a internet");
+    status = WiFi.begin("Totalplay-2.4G-f030", "32W6W96ptV8mPK2P");
+    
     delay(1000);
 	}
 }
 
-void ipsTry(){
-  for (int i = 0; i < config["server-number"]; i++){
+void serverTest(){
+  for (int i = 0; i < config["server_number"]; i++){
     
     if (status != WL_CONNECTED) {
       internetConnection();
@@ -80,41 +109,73 @@ void ipsTry(){
 
     int err = 0;
     if (config["servers"][i]["type"] == "http"){
-      String t = config["servers"][i]["type"];
-      String n = config["servers"][i]["name"];
-      Serial.print(t);
-      Serial.print("://");
-      Serial.println(n);
-      
+      //String t = config["servers"][i]["type"];
+      //String n = config["servers"][i]["name"];
+      //Serial.print(t);
+      //Serial.print("://");
+      //Serial.println(n);
+      Serial.print("Server: ");
+      Serial.println(i);
       for (int j = 1; j <= 5; j++) {
-        err = http.get(config["servers"][i]["name"], "/");
-
-        if (err == 0) {
-         break;
+        err = http.get(config["servers"][i]["name"], config["servers"][i]["endpoint"]);
+        Serial.println(i);
+        int severStatus = http.responseStatusCode();
+        if (severStatus > 0 && severStatus < 500) {
+          break;
         }
-        Serial.print("Intento: ");
-        Serial.println(j);
-        delay(2000);
+
+        //delay(2000); 
+        if (j == 5){
+          String serverName = config["servers"][i]["name"];
+            Serial.println("a");
+          if (client.connect("192.168.100.10", 3000)) {
+            Serial.print("Error en ");
+            Serial.println(serverName);
+            String errContent = "";
+            delay(2000);
+            msg["server"] = serverName;
+            msg["action"] = "report";
+            Serial.println("a");
+            serializeJson(msg, errContent);
+            char charBuf[256];
+            errContent.toCharArray(charBuf, 256);
+            client.write(charBuf);
+            Serial.println(charBuf);
+          }
+          client.stop();
+          Serial.println(serverName);
+        }
       }
       
+
     }else{
-      Serial.println("Se conecta por https");
-      
       for (int j = 1; j <= 5; j++) {
-        err = http.get(config["servers"][i]["name"], "/");
-        String t = config["servers"][i]["type"];
-         String n = config["servers"][i]["name"];
-        Serial.print(t);
-        Serial.print("://");
-        Serial.println(n);
-        
+        err = http.get(config["servers"][i]["name"], config["servers"][i]["endpoint"]);
         if (err == 0) {
           break;
         }
 
-        Serial.print("Intento: ");
-        Serial.println(j);
-        delay(2000);
+        //delay(2000);
+        if (j==5){
+          String serverName = config["servers"][i]["name"];
+          if (client.connect("192.168.100.10", 3000)) {
+           
+            Serial.print("Error en ");
+            Serial.println(serverName);
+            String errContent = "";
+            delay(2000);
+            msg["action"] = "report";
+            msg["server"] = serverName;
+            serializeJson(msg, errContent);
+            char charBuf[256];
+            errContent.toCharArray(charBuf, 256);
+            client.write(charBuf);
+            Serial.println(charBuf);
+          }
+          client.stop();
+          
+          
+        }
       }
     }
   }
